2844cab8adc6ddef48724d38b07d1fece42d9d2e
yangrui.0 <yangrui.0@bytedance.com>
2025-09-18 14:34:48 +0800

    feat: up

--- rust/shell.nix
+++ rust/shell.nix
@@ -1,35 +1,33 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/fd487183437963a59ba763c0cc4f27e3447dd6dd.tar.gz";
-  rust-overlay = import (
-    builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/10d4529b7ead35863caa77993915104345524bed.tar.gz"
-  );
+  rust-overlay = fetchTarball "https://github.com/oxalica/rust-overlay/archive/10d4529b7ead35863caa77993915104345524bed.tar.gz";
   pkgs = import nixpkgs {
     config = { };
     overlays = [
-      rust-overlay
-      (final: prev: {
-        rustToolchain =
-          let
-            rust = prev.rust-bin;
-          in
-          if builtins.pathExists ./rust-toolchain.toml then
-            rust.fromRustupToolchainFile ./rust-toolchain.toml
-          else if builtins.pathExists ./rust-toolchain then
-            rust.fromRustupToolchainFile ./rust-toolchain
-          else
-            rust.stable.latest.default.override {
-              extensions = [
-                "rust-src"
-                "rustfmt"
-              ];
-            };
-      })
+      (import rust-overlay)
     ];
   };
+
+  rust-tool-chain =
+    let
+      rust = pkgs.rust-bin;
+    in
+    if builtins.pathExists ./rust-toolchain.toml then
+      rust.fromRustupToolchainFile ./rust-toolchain.toml
+    else if builtins.pathExists ./rust-toolchain then
+      rust.fromRustupToolchainFile ./rust-toolchain
+    else
+      rust.stable.latest.default.override {
+        extensions = [
+          "rust-src"
+          "rustfmt"
+        ];
+      };
 in
 pkgs.mkShell {
   packages = with pkgs; [
-    rustToolchain
+    rust-tool-chain
+
     openssl
     pkg-config
     cargo-deny
@@ -40,6 +38,6 @@ pkgs.mkShell {
 
   env = {
     # Required by rust-analyzer
-    RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+    RUST_SRC_PATH = "${rust-tool-chain}/lib/rustlib/src/rust/library";
   };
 }
